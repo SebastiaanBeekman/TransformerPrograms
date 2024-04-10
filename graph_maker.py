9003f24b2fced88fcca9b1858577b9e9875df7e3
Sebastiaan Beekman <sebas.beekman@gmail.com>
2024-04-10 20:34:24 +0200

    grapher

--- graph_maker.py
+++ graph_maker.py
@@ -1,6 +1,6 @@
 import re
 import numpy as np
-import pandas as pd
+import matplotlib.pyplot as plt
 
 train_acc_pattern = re.compile(r'train: loss=\d+\.\d+, acc=([\d.]+),')
 test_acc_pattern = re.compile(r'test: loss=\d+\.\d+, acc=([\d.]+),')
@@ -18,10 +18,10 @@ def get_acc(file):
 
 if __name__ == "__main__":
     files = ["double_hist", "dyck1", "dyck2", "hist", "mostfreq", "reverse", "sort"]
+    train_values = {file: [[], []] for file in files}
+    x = [10, 20, 30 , 40]
+    
     for file in files:
-        train_values = []
-        test_values = []
-        
         for i in range(1, 5):
             acc = np.zeros((5, 2))
             for j in range(5):
@@ -30,8 +30,19 @@ if __name__ == "__main__":
                 with open(log_file_path, 'r') as f:
                     acc[j] = get_acc(f)
 
-            train_values.append(np.round([np.mean(acc[:, 0]), acc[np.argmax(acc[:, 1]), 0], acc[np.argmin(acc[:, 1]), 0], np.median(acc[:, 0])], 4))
-            test_values.append(np.round([np.mean(acc[:, 1]), np.max(acc[:, 1]), np.min(acc[:, 1]), np.median(acc[:, 1])], 4))
+            train_values[file][0].append(np.round(np.mean(acc[:, 1]), 4))
+            train_values[file][1].append(np.round(np.std(acc[:, 1]), 4))
+    
+        plt.errorbar(x, train_values[file][0], yerr=train_values[file][1], label=file, marker='o', capsize=5)
 
-        df = pd.DataFrame({'': ['Average', 'Max', 'Min', "median"], 'train 1-10': train_values[0], 'test 1-10': test_values[0], 'train 1-20': train_values[1], 'test 21-30': test_values[1], 'train 1-30': train_values[2], 'test 31-40': test_values[2], 'train 1-40': train_values[3], 'test 41-50': test_values[3]})
-        df.to_excel(f'data/excel/{file}.xlsx', index=False, float_format='%.4f')
+    plt.rcParams.update({'font.size': 14})
+    plt.xticks(x)
+    plt.yticks(np.arange(0, 1.1, 0.1))
+    
+    plt.xlabel('Train Length', fontsize=14)
+    plt.ylabel('Accuracy (%)', fontsize=14)
+    plt.title('Length generalization: Test accuracy over different lengths')
+    plt.grid(True)
+    plt.tight_layout()
+    plt.legend(loc='lower right')
+    plt.show()
